e9fbe50bf2635ca1d5c96a8b945fd9e51c6f144a
Ruben Taelman <ruben.taelman@ugent.be>
2018-07-24 15:32:37 +0200

    Add jsonld-context-parse bin

---
+++ bin/jsonld-context-parse.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+import {readFileSync} from "fs";
+import {ContextParser} from "../lib/ContextParser";
+
+const argv = process.argv.slice(2);
+
+if (argv.length !== 2) {
+  process.stderr.write(`jsonld-context-parse parses JSON-LD contexts
+
+Usage:
+  jsonld-context-parse url http://json-ld.org/contexts/person.jsonld
+  jsonld-context-parse file path/to/context.jsonld
+  jsonld-context-parse arg '{ "xsd": "http://www.w3.org/2001/XMLSchema#" }'
+`);
+  process.exit(1);
+}
+
+const type = argv[0];
+
+let input: any;
+switch (type) {
+case 'url':
+  input = argv[1];
+  break;
+case 'file':
+  input = JSON.parse(readFileSync(argv[1], 'utf8'));
+  break;
+case 'arg':
+  input = JSON.parse(argv[1]);
+  break;
+default:
+  process.stderr.write(`Unknown context type. Choose from url, file or arg.`);
+  process.exit(1);
+  break;
+}
+
+new ContextParser().parse(input)
+  .then((context) => {
+    process.stdout.write(JSON.stringify(context, null, '  '));
+    process.stdout.write('\n');
+  })
+  .catch((err) => {
+    process.stderr.write(err);
+  });
